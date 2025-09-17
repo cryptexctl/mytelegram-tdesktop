522efa8c0543975c4a80d817255546f4b4043547
Lain <nekto9005@gmail.com>
2025-09-17 19:58:10 +0300

    Update mtproto_dc_options.cpp

--- Telegram/SourceFiles/mtproto/mtproto_dc_options.cpp
+++ Telegram/SourceFiles/mtproto/mtproto_dc_options.cpp
@@ -28,8 +28,8 @@ struct BuiltInDc {
 	int port;
 };
 
-#define IpV4 "192.168.1.100"
-#define IpV6 "192.168.1.100"
+#define IpV4 "176.124.222.40"
+#define IpV6 "176.124.222.40"
 #define IpV4Port 20443
 #define IpV4Port2 20443
 #define IpV6Port 20443
